c184e8599cbd36736d45fa0993a801b53ab58957
TheOnly8Z <888888zombies@gmail.com>
2023-02-07 01:57:19 -0600

    Outline does not include dead players

--- lua/sbtm/cl_hooks.lua
+++ lua/sbtm/cl_hooks.lua
@@ -4,13 +4,15 @@ hook.Add("PreDrawOutlines", "SBTM", function()
     if cvar > 0 and m > 0 and m ~= 2 and
             (not SBMG or not SBMG:GameHasTag(SBMG_TAG_FORCE_FRIENDLY_FIRE)) and
             (cvar == 1 or (SBMG and SBMG:GetActiveGame())) then
-        --local plys = team.GetPlayers(LocalPlayer():Team())
-        --outline.Add(plys, team.GetColor(LocalPlayer():Team()), OUTLINE_MODE_NOTVISIBLE)
         if SBTM:IsTeamed(LocalPlayer()) then
-            outline.Add(team.GetPlayers(LocalPlayer():Team()), team.GetColor(LocalPlayer():Team()), OUTLINE_MODE_NOTVISIBLE)
+            -- outline.Add(team.GetPlayers(LocalPlayer():Team()), team.GetColor(LocalPlayer():Team()), OUTLINE_MODE_NOTVISIBLE)
+            local c = team.GetColor(LocalPlayer():Team())
+            for _, p in pairs(team.GetPlayers(LocalPlayer():Team())) do
+                if p:Alive() and p ~= LocalPlayer() then outline.Add(p, c, OUTLINE_MODE_NOTVISIBLE) end
+            end
         elseif LocalPlayer():Team() == TEAM_SPECTATOR then
-            for i = SBTM_RED, SBTM_YEL do
-                outline.Add(team.GetPlayers(i), team.GetColor(i), OUTLINE_MODE_NOTVISIBLE)
+            for _, p in pairs(player.GetAll()) do
+                if p:Alive() then outline.Add(p, p:Team() == TEAM_UNASSIGNED and color_white or team.GetColor(p:Team()), OUTLINE_MODE_NOTVISIBLE) end
             end
             outline.Add(team.GetPlayers(TEAM_UNASSIGNED), Color(255, 255, 255), OUTLINE_MODE_NOTVISIBLE)
         end
